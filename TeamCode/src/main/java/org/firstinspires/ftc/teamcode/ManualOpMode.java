eae7740823170b4adddd2c4ebcc146fd281140a6
wrkzk <wrkzk@protonmail.com>
2022-01-21 08:02:19 -0600

    Add teleop annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
 public class ManualOpMode extends LinearOpMode {
     public void runOpMode() {}
 }
