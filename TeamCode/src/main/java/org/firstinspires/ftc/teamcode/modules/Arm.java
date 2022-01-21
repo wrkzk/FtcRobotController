e60bb23c137590f2bb2d0954faafc9d2c2f4919d
wrkzk <wrkzk@protonmail.com>
2022-01-21 08:02:08 -0600

    Finish basic arm functionality

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Arm.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Arm {
+
+    private Servo scoop = hardwareMap.get(DcMotor.class, "scoop");
+    private DcMotor arm = hardwareMap.get(DcMotor.class, "arm");
+    private int currentArmState = 1;
+
+    private final double scoopOpenPos = 0.085;
+    private final double scoopClosedPos = 0.3;
+    private final double scoopDropPos = 0.9;
+
+    private final int stageOneTicks = 80;
+    private final int stageTwoTicks = 595;
+
+    private final double stageOnePower = 0.5;
+    private final double stageTwoPower = 0.7;
+    private final double armDownPower = 0.2;
+
+    public Arm() {
+        scoop.setPosition(scoopOpenPos);
+    }
+
+    public void armUpStageOne() {
+        if (currentArmState == 1) {
+            scoop.setPosition(scoopClosedPos);
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setTargetPosition(-stageOneTicks);
+            arm.setPower(stageOnePower);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            currentArmState = 2;
+        }
+    }
+
+    public void armUpStageTwo() {
+        if (currentArmState == 2) {
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setTargetPosition(-stageTwoTicks);
+            arm.setPower(stageTwoPower);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            if (!arm.isBusy()) {
+                scoop.setPosition(scoopDropPos);
+                currentArmState = 3;
+            }
+        }
+    }
+
+    public void armDownStageOne() {
+        if (currentArmState == 3) {
+            scoop.setPosition(scoopClosedPos);
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setTargetPosition(stageTwoTicks);
+            arm.setPower(armDownPower);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            currentArmState = 2;
+        }
+    }
+
+    public void armDownStageTwo() {
+        if (currentArmState == 2) {
+            scoop.setPosition(scoopOpenPos);
+            arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            arm.setTargetPosition(stageOneTicks);
+            arm.setPower(armDownPower);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            currentArmState = 1;
+        }
+    }
+
+    public void update() {
+        if (gamepad1.right_bumper && currentArmState == 1) {
+            armUpStageOne();
+        } else if (gamepad1.right_bumper && currentArmState == 2) {
+            armUpStageTwo();
+        } else if (gamepad1.left_bumper && currentArmState == 2) {
+            armDownStageTwo();
+        } else if (gamepad1.left_bumper && currentArmState == 3) {
+            armDownStageOne();
+        }
+    }
+}
