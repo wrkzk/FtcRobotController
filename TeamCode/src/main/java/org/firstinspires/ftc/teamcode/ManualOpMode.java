528ccf0746bc2bfda1db090f2a82f594bf222b0d
wrkzk <wrkzk@protonmail.com>
2022-01-21 09:18:55 -0600

    Basic structure of ManualOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -2,8 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.modules.Robot;
 
-@TeleOp
+@TeleOp(name = "Intake/Arm Test Mode", group = "")
 public class ManualOpMode extends LinearOpMode {
-    public void runOpMode() {}
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        Robot robot = new Robot();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            robot.update();
+        }
+    }
 }
