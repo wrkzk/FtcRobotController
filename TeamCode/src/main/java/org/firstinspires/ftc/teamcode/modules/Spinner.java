9609d8763a68eadc226732462e17b2b42cb60783
wrkzk <wrkzk@protonmail.com>
2022-01-26 15:12:42 -0600

    Create basic Spinner.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Spinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Spinner.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.modules;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Spinner {
+
+    private HardwareMap hwMap;
+    private Gamepad gamepad1;
+    private DcMotor spinner;
+
+    public Spinner(HardwareMap hwMap, Gamepad gamepad1) {
+        this.hwMap = hwMap;
+        this.gamepad1 = gamepad1;
+        this.spinner = hwMap.get(DcMotor.class, "spinner");
+    }
+
+    public void update() {
+        double spinnerPower = -gamepad1.right_trigger;
+        spinner.setPower(spinnerPower);
+    }
+
 }
