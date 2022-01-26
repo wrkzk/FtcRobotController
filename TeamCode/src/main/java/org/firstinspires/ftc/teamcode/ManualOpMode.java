62f23ad55c799c5def3464276cbcad1e8ec30de4
wrkzk <wrkzk@protonmail.com>
2022-01-26 15:15:06 -0600

    Add spinner and intake to ManualOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.modules.Arm;
 import org.firstinspires.ftc.teamcode.modules.Drivetrain;
+import org.firstinspires.ftc.teamcode.modules.Intake;
+import org.firstinspires.ftc.teamcode.modules.Spinner;
 
 @TeleOp(name = "Driver-Controlled OpMode", group = "")
 public class ManualOpMode extends LinearOpMode {
@@ -18,12 +20,16 @@ public class ManualOpMode extends LinearOpMode {
 
         Arm arm = new Arm(hwMap, gamepad1);
         Drivetrain drive = new Drivetrain(hwMap, gamepad1);
+        Intake intake = new Intake(hwMap, gamepad1);
+        Spinner spinner = new Spinner(hwMap, gamepad1);
 
         waitForStart();
 
         while (opModeIsActive()) {
             arm.update();
             drive.update();
+            intake.update();
+            spinner.update();
         }
     }
 }
