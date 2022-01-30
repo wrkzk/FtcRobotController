f2e3a42a68f62d3de4c83a04d6c3ba17b72d0d1d
wrkzk <wrkzk@protonmail.com>
2022-01-30 13:51:01 -0600

    Implement basic autonomous section

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -9,17 +9,20 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 
-@TeleOp(name = "Tensorflow Detection Test")
-public class TensorflowTestOpMode extends LinearOpMode {
+@TeleOp(name = "Autonomous OpMode")
+public class AutonomousOpMode extends LinearOpMode {
 
-    // Random
+    // ML model constants
     private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
     private static final String[] LABELS = { "Ball", "Cube", "Duck", "Marker" };
 
-    // Vuforia stuff
+    // Webcam constants
     private static final String VUFORIA_KEY = "AdKwFHr/////AAABmUrKa6jPRkCNg8oDY6tARNNXDktMphXL0xSR/B1J7j5Cfpi+yERJZsfKfoecmT+s4/cTg4A5d1X721HLFbZ0plZB1xJC3CUlh1mw2igecNnoSDKmfK5fOUFV8HlJt3325yDvjX5+PMRaQjEVdv6HbiUAL0tlH8/ffMP9rbY3UYrkiEnxK05/EKCRKnGQPn7DCDcbIaD/gS110uFMRn74vqksry9HkN0+WAZPznmyKMhqoIgfDjm8uP7E8uKviitRc6xpXuUIsUNndwdrXJ7NgM1BPraB/ILOgzXGqhAzS1JRKXkJVMyKQGrshOOFQxJYznbEhsCxe+RF/VC4Pj8jzfHUr0qUwCU3UbCdAnHAbgix";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
+
+    private boolean isDuckFound = false;
+    private int targetLevel;
 
     @Override
     public void runOpMode() {
@@ -29,8 +32,6 @@ public class TensorflowTestOpMode extends LinearOpMode {
 
         if (tfod != null) {
             tfod.activate();
-
-            // Set the tensorflow zoom and aspect ratio
             tfod.setZoom(2.5, 16.0/9.0);
         }
 
@@ -38,56 +39,57 @@ public class TensorflowTestOpMode extends LinearOpMode {
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                if (tfod != null) {
 
-                    // Get the updates list of recognitions from tensorflow (will return null if no new recognitions)
+                // Determine which level the duck has to go on
+                if (!isDuckFound && tfod != null) {
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-
-                        // Iterate through the list and display the bounding information
-                        int i = 0;
+                    while (updatedRecognitions != null) {
                         for (Recognition recognition : updatedRecognitions) {
-                            telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                            telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f", recognition.getLeft(), recognition.getTop());
-                            telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f", recognition.getRight(), recognition.getBottom());
-                            i++;
+                            if (recognition.getLabel().equals("Block") || recognition.getLabel().equals("Duck")) {
+                                Recognition duckRecognition = recognition;
+                                float totalWidth = duckRecognition.getLeft() + duckRecognition.getRight();
+                                if (duckRecognition.getLeft() < totalWidth / 3) {
+                                    targetLevel = 1;
+                                } else if (duckRecognition.getLeft() > totalWidth / 3 && duckRecognition.getLeft() < totalWidth * ((float)2 / 3)) {
+                                    targetLevel = 2;
+                                } else if (duckRecognition.getLeft() > totalWidth * ((float)2 / 3)) {
+                                    targetLevel = 3;
+                                }
+                                isDuckFound = true;
+                            }
                         }
-
-                        // Update the telemetry
-                        telemetry.update();
+                        updatedRecognitions = tfod.getUpdatedRecognitions();
                     }
                 }
+
+                // Drive forward a small amount
+                // Turn right 90 degrees
+                // Drive forward until on same level as the shipping hub
+                // Turn right 90 degrees
+                // Back into the shipping hub (distance might depend on what the targetLevel is)
+                // Drive forward a small amount
+                // Turn left 90 degrees
+                // Drive forward until robot is completely inside of the
             }
         }
     }
 
     // Initialize Vuforia
     private void initVuforia() {
-        // Create a Parameters object to initialize Vuforia
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        // Pass the Vuforia key and the webcam name to the parameters object
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
         parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
-
-        //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
     }
 
     // Initialize Tensorflow
     private void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
-        // Create a Parameters object for tensorflow
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-
-        // Pass in the parameters to the tensorflow parameters object, and create the tensorflow instance
         tfodParameters.minResultConfidence = 0.8f;
         tfodParameters.isModelTensorFlow2 = true;
         tfodParameters.inputSize = 320;
         tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-
-        // Load the trained tensorflow model
         tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
     }
 }
