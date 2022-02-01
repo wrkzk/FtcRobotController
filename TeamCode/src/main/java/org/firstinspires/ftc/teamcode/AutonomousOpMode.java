754a26d0c59727d97621a34289bd9226e2d2e5f8
wrkzk <wrkzk@protonmail.com>
2022-02-01 08:35:05 -0600

    Finish autonomous section

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -1,80 +1,128 @@
 package org.firstinspires.ftc.teamcode;
 
-import java.util.List;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
 
-@TeleOp(name = "Autonomous OpMode")
+import org.firstinspires.ftc.teamcode.modules.Arm;
+import org.firstinspires.ftc.teamcode.modules.Drivetrain;
+import java.lang.InterruptedException;
+
+
+@Autonomous(name = "Autonomous OpMode")
 public class AutonomousOpMode extends LinearOpMode {
 
-    // ML model constants
     private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
-    private static final String[] LABELS = { "Ball", "Cube", "Duck", "Marker" };
+    private static final String[] LABELS = {"Ball", "Cube", "Duck", "Marker"};
 
-    // Webcam constants
     private static final String VUFORIA_KEY = "AdKwFHr/////AAABmUrKa6jPRkCNg8oDY6tARNNXDktMphXL0xSR/B1J7j5Cfpi+yERJZsfKfoecmT+s4/cTg4A5d1X721HLFbZ0plZB1xJC3CUlh1mw2igecNnoSDKmfK5fOUFV8HlJt3325yDvjX5+PMRaQjEVdv6HbiUAL0tlH8/ffMP9rbY3UYrkiEnxK05/EKCRKnGQPn7DCDcbIaD/gS110uFMRn74vqksry9HkN0+WAZPznmyKMhqoIgfDjm8uP7E8uKviitRc6xpXuUIsUNndwdrXJ7NgM1BPraB/ILOgzXGqhAzS1JRKXkJVMyKQGrshOOFQxJYznbEhsCxe+RF/VC4Pj8jzfHUr0qUwCU3UbCdAnHAbgix";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
-
-    private boolean isDuckFound = false;
     private int targetLevel;
+    private double motorPower = 0.3;
 
     @Override
-    public void runOpMode() {
-        // Initialize the Vuforia localizer (for video frames) and tensorflow
+    public void runOpMode() throws InterruptedException {
+
         initVuforia();
         initTfod();
 
         if (tfod != null) {
             tfod.activate();
-            tfod.setZoom(2.5, 16.0/9.0);
+            tfod.setZoom(1, 16.0/9.0);
         }
+
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
+
+        boolean duckFound = false;
+        int numOfPasses = 0;
+
+        Arm arm = new Arm(hardwareMap, gamepad1);
+        Drivetrain drive = new Drivetrain(hardwareMap, gamepad1);
+        arm.armUpStageOne();
 
         waitForStart();
 
         if (opModeIsActive()) {
-            while (opModeIsActive()) {
 
-                // Determine which level the duck has to go on
-                if (!isDuckFound && tfod != null) {
+            while (!duckFound) {
+                if (tfod != null) {
                     List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    while (updatedRecognitions != null) {
-                        for (Recognition recognition : updatedRecognitions) {
-                            if (recognition.getLabel().equals("Block") || recognition.getLabel().equals("Duck")) {
-                                Recognition duckRecognition = recognition;
-                                float totalWidth = duckRecognition.getLeft() + duckRecognition.getRight();
-                                if (duckRecognition.getLeft() < totalWidth / 3) {
-                                    targetLevel = 1;
-                                } else if (duckRecognition.getLeft() > totalWidth / 3 && duckRecognition.getLeft() < totalWidth * ((float)2 / 3)) {
-                                    targetLevel = 2;
-                                } else if (duckRecognition.getLeft() > totalWidth * ((float)2 / 3)) {
-                                    targetLevel = 3;
+
+                    if (numOfPasses > 1000) {
+                        if (updatedRecognitions != null) {
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals("Duck")) {
+                                    if (recognition.getLeft() < 200) {
+                                        targetLevel = 2;
+                                        if (numOfPasses > 1500) {
+                                            duckFound = true;
+                                        }
+                                    } else if (recognition.getLeft() > 200) {
+                                        targetLevel = 3;
+                                        if (numOfPasses > 1500) {
+                                            duckFound = true;
+                                        }
+                                    }
                                 }
-                                isDuckFound = true;
+                            }
+                            if (duckFound == false) {
+                                targetLevel = 1;
+                                if (numOfPasses > 1500) {
+                                    duckFound = true;
+                                }
                             }
                         }
-                        updatedRecognitions = tfod.getUpdatedRecognitions();
                     }
+                    numOfPasses++;
+
                 }
 
-                // Drive forward a small amount
-                // Turn right 90 degrees
-                // Drive forward until on same level as the shipping hub
-                // Turn right 90 degrees
-                // Back into the shipping hub (distance might depend on what the targetLevel is)
-                // Drive forward a small amount
-                // Turn left 90 degrees
-                // Drive forward until robot is completely inside of the
+                telemetry.addData("Target Level", targetLevel);
+                telemetry.update();
             }
+
+            drive.driveStraight(75, motorPower);
+            //drive.resetMotors();
+            Thread.sleep(200);
+            drive.turnRight(250, motorPower);
+            //drive.resetMotors();
+            Thread.sleep(200);
+            drive.driveStraight(400, motorPower);
+            //drive.resetMotors();
+            Thread.sleep(200);
+            drive.turnRight(300, motorPower);
+            //drive.resetMotors();
+            Thread.sleep(200);
+            drive.driveReverse(110, motorPower);
+            //drive.resetMotors();
+
+            drive.driveReverse(1, 0.01);
+
+            if (targetLevel == 1) {
+                arm.armUpLevelOne();
+            } else if (targetLevel == 2) {
+                arm.armUpLevelTwo();
+            } else if (targetLevel == 3) {
+                arm.armUpLevelThree();
+            }
+
+            Thread.sleep(200);
+            drive.turnLeft(280, motorPower);
+            Thread.sleep(200);
+            drive.driveStraight(1500, 0.5);
+            arm.armDownStageTwo();
         }
     }
 
-    // Initialize Vuforia
     private void initVuforia() {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
@@ -82,7 +130,6 @@ public class AutonomousOpMode extends LinearOpMode {
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
     }
 
-    // Initialize Tensorflow
     private void initTfod() {
         int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
