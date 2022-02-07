dc2e28dd593e04d08a0a9aaa73179cdb5f0f4757
wrkzk <wrkzk@protonmail.com>
2022-02-07 14:29:12 -0600

    Enable multithreading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -2,10 +2,51 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.modules.Arm;
 import org.firstinspires.ftc.teamcode.modules.Drivetrain;
 import org.firstinspires.ftc.teamcode.modules.Intake;
 import org.firstinspires.ftc.teamcode.modules.Spinner;
+
+class DrivetrainThread extends Thread {
+
+    Drivetrain drive;
+
+    public DrivetrainThread(Drivetrain drive) {
+        this.drive = drive;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                drive.update();
+            } catch (Exception e) {}
+        }
+    }
+}
+
+class ManipulatorThread extends Thread {
+
+    Arm arm;
+    Intake intake;
+    Spinner spinner;
+
+    public ManipulatorThread(Arm arm, Intake intake, Spinner spinner) {
+        this.arm = arm;
+        this.intake = intake;
+        this.spinner = spinner;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                arm.update();
+                intake.update();
+                spinner.update();
+            } catch (Exception e) {}
+        }
+    }
+}
 
 @TeleOp(name = "Driver-Controlled OpMode", group = "")
 public class ManualOpMode extends LinearOpMode {
@@ -15,18 +56,24 @@ public class ManualOpMode extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        Arm arm = new Arm(hardwareMap, gamepad1);
+        Arm arm = new Arm(hardwareMap, gamepad2);
         Drivetrain drive = new Drivetrain(hardwareMap, gamepad1);
-        Intake intake = new Intake(hardwareMap, gamepad1);
-        Spinner spinner = new Spinner(hardwareMap, gamepad1);
+        Intake intake = new Intake(hardwareMap, gamepad2);
+        Spinner spinner = new Spinner(hardwareMap, gamepad2);
+
+        DrivetrainThread drivetrain = new DrivetrainThread(drive);
+        ManipulatorThread manipulators = new ManipulatorThread(arm, intake, spinner);
 
         waitForStart();
 
-        while (opModeIsActive()) {
-            arm.update();
-            drive.update();
-            intake.update();
-            spinner.update();
+        if (opModeIsActive()) {
+            drivetrain.start();
+            manipulators.start();
         }
+
+        while (opModeIsActive());
+
+        drivetrain.stop();
+        manipulators.stop();
     }
 }
