b9be1c4c504ff61250748d80a8279d2ea3e2e845
wrkzk <wrkzk@protonmail.com>
2022-02-08 10:33:36 -0600

    Refactor threads and write comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -1,75 +1,64 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.ArrayList;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+// Import the modules for the hardware
 import org.firstinspires.ftc.teamcode.modules.Arm;
 import org.firstinspires.ftc.teamcode.modules.Drivetrain;
 import org.firstinspires.ftc.teamcode.modules.Intake;
 import org.firstinspires.ftc.teamcode.modules.Spinner;
+import org.firstinspires.ftc.teamcode.modules.Component;
 
 @TeleOp(name = "Driver-Controlled OpMode", group = "")
 public class ManualOpMode extends LinearOpMode {
 
-    class DrivetrainThread extends Thread {
+    // General thread to control groups of components
+    class ComponentThread extends Thread {
+        Component[] components;
 
-        Drivetrain drive;
-
-        public DrivetrainThread(Drivetrain drive) {
-            this.drive = drive;
+        public ComponentThread(Component[] components) {
+            this.components = components;
         }
 
         public void run() {
             while (opModeIsActive()) {
                 try {
-                    drive.update();
-                } catch (Exception e) {}
-            }
-        }
-    }
-
-    class ManipulatorThread extends Thread {
-
-        Arm arm;
-        Intake intake;
-        Spinner spinner;
-
-        public ManipulatorThread(Arm arm, Intake intake, Spinner spinner) {
-            this.arm = arm;
-            this.intake = intake;
-            this.spinner = spinner;
-        }
-
-        public void run() {
-            while (opModeIsActive()) {
-                try {
-                    arm.update();
-                    intake.update();
-                    spinner.update();
-                } catch (Exception e) {}
+                    for (Component component : components) {
+                        component.update();
+                    }
+                } catch (Exception e) {
+                    telemetry.addData("Status", "Error: " + e);
+                    telemetry.update();
+                }
             }
         }
     }
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
 
+        // Initialize each piece of hardware
         Arm arm = new Arm(hardwareMap, gamepad2);
         Drivetrain drive = new Drivetrain(hardwareMap, gamepad1);
         Intake intake = new Intake(hardwareMap, gamepad2);
         Spinner spinner = new Spinner(hardwareMap, gamepad2);
 
-        // Initialize two threads, one for updating the drivetrain, and one for the manipulator
-        DrivetrainThread drivetrain = new DrivetrainThread(drive);
-        ManipulatorThread manipulators = new ManipulatorThread(arm, intake, spinner);
+        // Initialize the drivetrain thread and the manipulators thread
+        ComponentThread drivetrainThread = new ComponentThread(new Component[] {drive});
+        ComponentThread manipulatorThread = new ComponentThread(new Component[] {arm, intake, spinner});
+
+        // Alert the user that the program has initialized
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
         waitForStart();
 
+        // Once the user starts the program, start the threads for each subsystem
         if (opModeIsActive()) {
-            drivetrain.start();
-            manipulators.start();
+            drivetrainThread.start();
+            manipulatorThread.start();
         }
     }
 }
